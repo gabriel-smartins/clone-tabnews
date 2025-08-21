512c77aa4c1e4059a2ef47e092fc33581ad977bf
Gabriel Martins <128493201+gabriel-smartins@users.noreply.github.com>
2025-08-21 20:25:50 +0000

    configurando o prettier

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-#**Bem-Vindo ao Tab News Clone!!!**
+# **Bem-Vindo ao Tab News Clone!!!**
 
 Implementação do https://www.tabnews.com.br/ para fins didáticos.
-
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 const Home = () => {
+  return (
+    <>
+      <h1>Jesus te ama! E eu também!</h1>
+    </>
+  );
+};
 
-    return (
-        <>
-        <h1>Jesus te ama! E eu também!</h1>
-        </>
-    )
-
+function teste() {
+  console.log("olá mundo");
 }
 
-export default Home
+export default Home;
