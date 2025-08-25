46ea81f70060060e7699b381d2803b9b2706f57a
Gabriel Martins <gab.smartinn@outlook.com>
2025-08-25 14:41:27 -0300

    add  endpoint

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-const somar = (numero1, numero2) => {
-  if (typeof numero1 != "number" || typeof numero2 != "number") {
-    return "Erro";
-  }
-  return numero1 + numero2;
-};
-
-exports.somar = somar;
