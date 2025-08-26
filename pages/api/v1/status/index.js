303b363d4106c0045125fe366b4ca10f3baf2a3d
Gabriel Martins <128493201+gabriel-smartins@users.noreply.github.com>
2025-08-26 20:22:30 +0000

    finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = postgresVersion.rows[0].server_version;
+
+  const databaseMaxCons = await database.query("SHOW max_connections;");
+  const databaseMaxConsValue = databaseMaxCons.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedCons = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenedConsValue = databaseOpenedCons.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConsValue),
+        opened_connections: databaseOpenedConsValue,
+      },
+    },
+  });
 };
 
 export default status;
